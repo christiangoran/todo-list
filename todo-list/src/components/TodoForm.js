c6b2222595feb04d3ecad2caaca8b0ed63b45b99
christiangoran <christian.goran@protonmail.com>
2024-01-15 10:54:31 +0100

    add components and connect to app.js

---
+++ todo-list/src/components/TodoForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function TodoForm() {
+  return (
+    <form>
+      <input type="text" placeholder="...add an item" />
+      <button type="submit">Add</button>
+    </form>
+  );
+}
+
+export default TodoForm;
